80d24f6834887efa4c72e4f6cc36efd1e8781167
Hassaan-Elahi <hassaan.elahi15@gmail.com>
2018-11-25 10:04:45 +0500

    added listener

--- app/src/main/java/marker/project/dineout/MainActivity.java
+++ app/src/main/java/marker/project/dineout/MainActivity.java
@@ -2,6 +2,8 @@ package marker.project.dineout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button end_button = findViewById(R.id.end_button);
+
+        end_button.setOnClickListener( new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String email = "hassaan.elahi15@gmail.com";
+
+            }
+        });
+
+
+
+
+
     }
+
+
 }
